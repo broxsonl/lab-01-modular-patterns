ebd783e1df9fc36e0514306c9354e2bd0f4ab7b7
Lee Broxson <broxsonl@gmail.com>
2016-09-13 18:57:09 -0700

    Changed the author email and the version number in pack.json.

--- lab-lee/gulpfile.js
+++ lab-lee/gulpfile.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+gulp.task('hello', function() {
+  console.log('hello world');
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('watch:hello', function() {
+  gulp.watch('**/*.js', ['hello']);
+});
